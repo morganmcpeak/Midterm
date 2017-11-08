04adf2f2ae280ebc4978c6c9b661fd7bd64ea900
lisamgerken <32423479+lisamgerken@users.noreply.github.com>
2017-11-08 13:27:25 -0500

    Update script.js
    random shuffle generator

--- midterm/script.js
+++ midterm/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
+  $("#start").on("click", function() {
+    var parent = $(".cardHolder");
+    var card = parent.child();
+    while (card.length) {
+      parent.append(card.slice(Math.floor(Math.random() * card.length), 1)[0]);
+    }
+  });
 
 
 
