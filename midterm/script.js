b0b019d5a15137f19995681e98ffc7942a8ed927
Hilary Wilson <32081469+hwilson2563@users.noreply.github.com>
2017-11-09 09:07:26 -0500

    Update script.js
    refining the process of checking the cards content

--- midterm/script.js
+++ midterm/script.js
@@ -29,7 +29,31 @@ $(document).ready(function(){
     cardOne = this.innerHTML;
     console.log("hey" + cardOne);   
   });
-
+//  check this out... we do not need the .off to keep going because our clicks are getting reset everytime
+//   $(".back").on("click", function() {
+//       if (clicks === 1) {
+//             cardTwo = this.innerHTML;
+//             console.log(cardTwo); 
+//             clicks++; 
+//         if (cardOne !== cardTwo){
+//             clicks = 0;
+//             console.log("yo no good");
+//             return;
+//           }
+//         if(cardOne.innerHTML === cardTwo.innerHTML) {
+//         $(".back").off("click");
+//         //clicks = 0;
+//         console.log("hello");
+//         return;
+//         } 
+//         } 
+//       if (clicks === 0) {
+//         clicks++;
+//         cardOne = this.innerHTML;
+//         console.log(cardOne); 
+//       }
+       
+//   });
 
 
 
