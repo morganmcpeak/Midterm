254be3b3929027d551e83bc30267eeefaaf556e3
lisamgerken <32423479+lisamgerken@users.noreply.github.com>
2017-11-09 14:06:37 -0500

    Update script.js
    changed locations for audio, set up auto play

--- midterm/script.js
+++ midterm/script.js
@@ -53,5 +53,12 @@ $(document).ready(function(){
        
       }    
   });
+  
+  myAudio = new Audio('thexfiles.mp3'); 
+	myAudio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+	}, false);
+	myAudio.play();
 
 });
